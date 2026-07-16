6593de3adc9f5c538467f843ad2e830f7bc931cc
Amilgiter <1204282697@qq.com>
2026-07-16 14:03:55 +0800

    Site updated: 2026-07-16 14:03:55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1784180802239";const preCache = ["/images/taichi.png","/images/banner.svg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1784181834829";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
